818d1cdc0b6d680f7ad504dcd0a377c94b407dd9
Panteha <panteha.ahmadi@gmail.com>
2017-05-04 18:48:40 +0100

    Add fizzbuzz test

--- lib/fizzbuzz2.rb
+++ lib/fizzbuzz2.rb
@@ -1,5 +1,6 @@
 class Integer
 	def fizzbuzz2
+		return "fizzbuzz" if self % 3 == 0 && self % 5 == 0
 		return "fizz" if self % 3 == 0
 		return "buzz" if self % 5 == 0
 	end
--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -1,12 +1,16 @@
 require "fizzbuzz2"
 describe "fizzbuzz test" do
 
-	it "returns fizz when passed 3" do
+	it "returns 'fizz' when passed 3" do
 		expect(3.fizzbuzz2).to eq "fizz"
 	end
 	
-	it "returns buzz when passed 5" do
+	it "returns 'buzz' when passed 5" do
 		expect(5.fizzbuzz2).to eq "buzz"
+	end
+
+	it "returns 'fizzbuzz' when passed 15" do
+		expect(15.fizzbuzz2).to eq "fizzbuzz"
 	end
 
 end
